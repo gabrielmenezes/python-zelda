597f9bb54380b88ba92eb915435212ad76ebbeae
Gabriel Soares <gabrielvm.soares@outlook.com>
2023-10-28 13:39:17 -0300

    Initial commit

---
+++ code/tile.py
@@ -0,0 +1,13 @@
+import pygame
+import os
+from settings import *
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(
+            os.path.join("graphics", "test", "rock.png")
+        ).convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -10)
